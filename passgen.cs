cd01a1363650b4d857132fc4b5549b2f5f28eefc
nyujuzer <107214326+nyujuzer@users.noreply.github.com>
2022-09-28 13:10:00 +0200

    Add files via upload

---
+++ passgen.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace orai
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            /*
+            string Progresbar = "This is animated title of Console";
+            var title = ""
+            while (true)
+            {
+                for (int i = 0; i < Progresbar.Length; i++)
+                {
+                    title += Progresbar[i];
+                    Console.Title = title;
+                    Thread.Sleep(100);
+                }
+                title = "";
+            }
+            int a = 0;
+            Console.Write("A Értéke: ");
+            string astr = Console.ReadLine();
+            Console.Write("B Értéke: ");
+            string bstr = Console.ReadLine();
+ 
+            try
+            {
+                
+                a = int.Parse(astr);
+                int b = int.Parse(bstr);
+
+                Console.WriteLine(a + b);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Adj egy számot, ne betűt"+ ex);
+            }
+            #region Szia
+            switch (a)
+            {
+                default:
+                    break;
+            }
+            #endregion
+            #region 
+            do
+            {
+                Console.WriteLine($"Hello World, {a}");
+                a *= 2 ;
+            } while (a > 0);
+
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+
+
+
+            #endregion
+            */
+
+            Console.SetWindowSize(75, 30);
+            Console.ReadKey();
+        }
+    }
+}
